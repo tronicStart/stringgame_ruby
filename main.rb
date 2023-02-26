edbf39be791e729377e1a433afea882caca608df
Tronic <113149052+tronicStart@users.noreply.github.com>
2023-02-26 14:30:44 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,34 @@
-require_relative 'stringgame'
+require_relative 'strgame'
 
-saludo = StringGame.new("Test1")
-saludo.ini_name_game
-saludo.print("HolaMundo")
-gets()
+#Hacemos las declaraciones principales
+#para poder mover el caracter
+x = 50
+y = 10
+player = '@';
+
+move_char = StringGame.new("Mover caracter")
+move_char.ini_name_game
+
+while true
+  
+  move_char.goto_xy(x,y)
+  puts "#{player}"
+  
+  key_player = move_char.read_key
+  
+  case key_player
+  when 'w'
+    y -= 1
+  when 'a'
+    x -= 1
+  when 's'
+    y += 1
+  when 'd'
+    x += 1
+  when "\e" # Tecla ESC para salir del bucle
+    break
+  end
+  
+  move_char._clrscr
+  
+end
